53e5499829f0a06e867f95b70b6317da367ff0ad
laurhakob <laurhakob@gmail.com>
2025-02-22 02:13:07 +0400

    video thumbnails

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,6 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "image.mux.com",
-      },
-      {
-        protocol: "https",
         hostname: "ix15g5sk18.ufs.sh",
       },
     ],
